0f86431aacbd4815160c502340cb993e84c45905
AdrianTBZ <150660940+AdrianTBZ@users.noreply.github.com>
2024-10-28 14:45:39 +0100

    Delete tasks directory

--- tasks/src/V2_SerieB/FussballMannschaftTest.java
+++
@@ -1,9 +0,0 @@
-package V2_SerieB;
-
-public class FussballMannschaftTest {
-	public static void main(String[] args) {
-		Mannschaft mannschaft = new Mannschaft();
-		Menu menu = new Menu(mannschaft);
-		menu.start();
-	}
-}
