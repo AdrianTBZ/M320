27c82567b49b2464f5d11f556cd6dd1205911323
AdrianTBZ <150660940+AdrianTBZ@users.noreply.github.com>
2024-11-04 11:16:36 +0100

    Update Sparkonto.java

--- src/Sparkonto.java
+++ src/Sparkonto.java
@@ -1,13 +1,14 @@
 public class Sparkonto extends Konto {
 	private double zinsRate;
 
-	public Sparkonto(String kontonummer, double zinsRate) {
-		super(kontonummer);
+	public Sparkonto(String kontonummer, Waehrung waehrung, double zinsRate) {
+		super(kontonummer, waehrung);
 		this.zinsRate = zinsRate;
 	}
 
 	public void zinsenBuchen() {
-		double zinsen = getSaldo() * zinsRate;
+		double zinsen = getSaldo() * (zinsRate / 100);
 		geldEinzahlen(zinsen);
+		System.out.println("Zinsen von " + zinsen + " " + getWaehrung().getName() + " wurden auf das Sparkonto gutgeschrieben.");
 	}
 }
